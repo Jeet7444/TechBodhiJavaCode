1226edffe679cd65b89541c790fe13470b53ae1a
Jeet7444 <waghsatyajeet@gmail.com>
2021-12-26 12:08:52 +0530

    jeet

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -7,7 +7,8 @@ public class HelloWorld {
 	public static void main(String args[])
 	{
 		System.out.println("hello world");
-		
+	
+		System.out.println("firfst  nvjvjfv");
 		HelloWorld hw = new HelloWorld();
 		hw.add();
 		int b =hw.substarct(20, 10);
